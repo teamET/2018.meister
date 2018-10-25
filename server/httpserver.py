8bbeb8d0e11b60f146affa3a5a6bc1bca59ba06c
sakakendo <sakakendo0321@gmail.com>
2018-10-25 23:40:47 +0900

    remake directory

--- server/httpserver.py
+++ server/httpserver.py
@@ -11,5 +11,5 @@ def index():
 def run():
     app.run(debug=True,host='0.0.0.0',port=8080)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
--- server/main.py
+++ server/main.py
@@ -2,24 +2,27 @@
 from abc import ABCMeta,abstractmethod
 from icecream import ic
 from numba import jit
-import server as httpserver
+import httpserver
+import os
 import sys
 import socket
 import importlib
 import threading
 
-MOTOR_NUM=4
-pwms=[[0,0] for i in range(MOTOR_NUM)]
+MOTOR_NUM=8
+pwms=[0 for i in range(MOTOR_NUM)]
 mode=0
 
-class  SubUdpServer(threading.Thread):
+class  UdpServer():
     def __init__(self,shared_data):
-        super(SubUdpServer,self).__init__()
+#        super(SubUdpServer,self).__init__()
         self.UDP_IP=""
         self.UDP_PORT=5005
         self.backlog=10
         self.bufsize=1024
         self.data=shared_data
+    def __del__(self):
+        socket.close()
     def is_json(myjson):
         try:
             json_object = json.loads(myjson)
@@ -31,24 +34,25 @@ class  SubUdpServer(threading.Thread):
         print('===  Sub Thread Starts===')
         print("PORT",self.UDP_PORT)
         sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        with closing(sock):
-            sock.bind((self.UDP_IP,self.UDP_PORT))
-            while True:
-                cnt=0
-                mes=sock.recv(self.bufsize)
-                raw=mes.decode('utf-8')
-                print('mes',mes,'raw',raw)
-                if raw == 'q':
-                    print('Sub process is terminated')
-                    break
-                elif raw is not '' : #and  self.is_json(raw) ==True:
-                    pwm_str=raw.split(',')
-                    for pwm in map(int,pwm_str):
-                        self.data[cnt]=pwm
-                        cnt+=1
-                    print(self.data)
-                else:
-                    print('empty message or not json ')
+        sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+#        with closing(sock):
+        sock.bind((self.UDP_IP,self.UDP_PORT))
+        while True:
+            cnt=0
+            mes=sock.recv(self.bufsize)
+            raw=mes.decode('utf-8')
+            print('mes',mes,'raw',raw)
+            if raw == 'q':
+                print('Sub process is terminated')
+                break
+            elif raw is not '' : #and  self.is_json(raw) ==True:
+                pwm_str=raw.split(',')
+                for pwm in map(int,pwm_str):
+                    self.data[cnt]=pwm
+                    cnt+=1
+                print(self.data)
+            else:
+                print('empty message or not json ')
 #                time.sleep(1)
 
 class Motor(metaclass=ABCMeta):
@@ -78,19 +82,19 @@ class Motor(metaclass=ABCMeta):
         pass
 
 class PiMotor(Motor):
-    def __init__(self):
-        pigpio=importlib.util.find_spec("pigpio")
+    def __init__(self,pins):
+        pigpio=importlib.import_module("pigpio")
+#        pigpio=importlib.util.find_spec("pigpio")
         if pigpio is None:
             print("pigpio is not found")
             return None
-        else:
-            print("found pigpio")
         self.pi=pigpio.pi()
         print('initialized pin is ',pins)
         self.pins=pins
         for pins in self.pins:
             for pin in pins:
                 self.pi.set_mode(pin,pigpio.OUTPUT)
+        ic("pins",pins,self.pins)
 
     def drive(self,port,target_rate):
         self.current_rate[port]=target_rate
@@ -113,7 +117,7 @@ class PiMotor(Motor):
     @jit(nogil=True)
     def run(self):
         while True:
-            ic(pwms)
+#            ic(pwms)
             for i in range(4):
                 self.drive(i,pwms[i]);
 class Arm:
@@ -122,36 +126,47 @@ class Arm:
 
 class TxMotor(Motor):
     def __init__(self):
-        pass
+        return None
     def drive():
         pass
     def drive_pin():
         pass
 
+
+motor=None
+try:
+    motor=PiMotor([[14,15],[23,24],[8,7],[16,20]])
+except ModuleNotFoundError:
+    print("pigpio not found")
+except Exception as e:
+    print(e)
+
+try:
+    motor=TxMotor
+except Exception as e:
+    print('tx2 not found')
+
+udpserver=UdpServer(pwms)
 def main():
-    motor=PiMotor()
     if motor is None:
-        print("pigpio not found")
-        exit()
-
-    #udp
-    server=SubUdpServer(pwms)
-    server.setDaemon(True)
-    server.start()
-
+        print("gpio setting is not complete")
+        sys.exit()
     threads=[
             threading.Thread(target=motor.run),
-            threading.Thread(target=httpserver.run)]
+#            threading.Thread(target=httpserver.run),
+            threading.Thread(target=udpserver.run)]
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
 #    motor.run()
 
-
 if __name__ == '__main__':
     try:
         main()
-    except KeyboardInterupt:
+    except KeyboardInterrupt:
         print("system interupted")
-        sys.exit()
+        del udpserver
+        del httpserver
+        del motor
+        os.exit()
