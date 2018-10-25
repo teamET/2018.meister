1850f9bc4330059741e193b8a236df4adfc7fdac
sakakendo <sakakendo0321@gmail.com>
2018-10-25 22:38:38 +0900

    add darknet client

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
 import socket
+import cv2
+from pydarknet import Detector,Image
+from icecream import ic
 
 UDP_IP="localhost"
 UDP_PORT=5005
@@ -9,6 +12,17 @@ def send(message):
     sock.sendto(message.encode('utf-8'),(UDP_IP,UDP_PORT))
     sleep(0.01)
 
-send("hello world")
+if __name__ == "__main__":
+    send("hello world")
+    filename="hello.png"
+#    cap=cv2.VideoCapture(0)
+    cfg,weights,data="cfg/yolov3.cfg","yolov3.weights","cfg/coco.data"
+    net=Detector(bytes(cfg,encoding="utf-8"),bytes(weights,encoding="utf-8"),0,bytes(data,encoding="utf-8"))
+    while True:
+        ret,frame=cap.read()
+        net.detect(Image(filename))
+        send("0,0,0,0,0,0,0,0")
+
+
 
 
