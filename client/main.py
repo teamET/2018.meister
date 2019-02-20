59e86c3726c3e805511bca2af3276c0ae513e0f8
sakakendo <sakakendo0321@gmail.com>
2019-02-20 17:48:38 +0000

    add zed , darknet libraries

---
+++ client/main.py
@@ -0,0 +1,31 @@
+
+import socket
+import cv2
+from pydarknet import Detector,Image
+from icecream import ic
+import pyzed.sl as sl
+
+
+UDP_IP="localhost"
+UDP_PORT=50007
+
+def send(message):
+    print("message",message)
+    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    sock.sendto(message.encode('utf-8'),(UDP_IP,UDP_PORT))
+    sleep(0.01)
+
+if __name__ == "__main__":
+    send("hello world")
+    filename="hello.png"
+#    cap=cv2.VideoCapture(0)
+    cfg,weights,data="cfg/yolov3.cfg","yolov3.weights","cfg/coco.data"
+    net=Detector(bytes(cfg,encoding="utf-8"),bytes(weights,encoding="utf-8"),0,bytes(data,encoding="utf-8"))
+    while True:
+        ret,frame=cap.read()
+        net.detect(Image(filename))
+        send("0,0,0,0,0,0,0,0")
+
+
+
+
